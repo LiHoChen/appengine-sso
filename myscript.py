3a9003b6abf92e3c04835f13080a025e154b90c9
LiHoChen <took221@gmail.com>
2016-07-15 14:35:25 +0800

    2 commit

---
+++ myscript.py
@@ -0,0 +1,14 @@
+import os
+filename = os.environ.get('PYTHONSTARTUP')
+if filename and os.path.isfile(filename):
+    with open(filename) as fobj:
+        startup_file = fobj.read()
+    exec(startup_file)
+
+# qqqq stupid  qqqqq
+
+
+
+#afjkljadfjlad;jf
+
+# just test
